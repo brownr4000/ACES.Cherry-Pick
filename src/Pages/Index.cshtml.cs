181a31612383151a76c8ce272011f97eb4298562
bakyadevi <91570270+bakyadevi@users.noreply.github.com>
2021-10-06 14:45:36 -0700

    Update Index.cshtml.cs
    Added name Bakyadevi Ray to IndexModel Comment in Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -8,6 +8,9 @@ using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Bakyadevi Ray
+    /// </summary>
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
