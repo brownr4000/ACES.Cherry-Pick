fb8f866a5c1072812882a0e828972208ba029a88
Bakyadevi Ray <91570270+bakyadevi@users.noreply.github.com>
2021-10-07 00:24:07 -0700

    Test
    Testing

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -9,7 +9,7 @@ using ContosoCrafts.WebSite.Services;
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary>
-    /// Bakyadevi Ray 
+    /// Bakyadevi Ray
     /// </summary>
     ///
     ///<summary>
