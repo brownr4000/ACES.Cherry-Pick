415ac8daaf6517c17d1fe39ed0e430c6c9ad3973
Robert Brown <rbrown3@seattleu.edu>
2021-10-12 01:02:03 -0700

    Revert "Trying to merge conflict again - Bob Brown"
    This reverts commit f8da2aae2c8301ac07bd086b8517e71fe184b917.

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Logging;
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
 /// <summary>
-/// I love Seattle Dogs and pizza
+/// I love Seattle Dogs
 /// </summary>
 namespace ContosoCrafts.WebSite.Pages
 {
