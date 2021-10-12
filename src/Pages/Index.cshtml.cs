f8da2aae2c8301ac07bd086b8517e71fe184b917
Robert Brown <rbrown3@seattleu.edu>
2021-10-11 20:02:45 -0700

    Trying to merge conflict again - Bob Brown

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Logging;
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
 /// <summary>
-/// I love Seattle Dogs
+/// I love Seattle Dogs and pizza
 /// </summary>
 namespace ContosoCrafts.WebSite.Pages
 {
