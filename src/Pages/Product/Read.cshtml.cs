b4b1d5314dc714eaa3247985076bc71e33f05013
Bakyadevi Ray <91570270+bakyadevi@users.noreply.github.com>
2021-10-19 12:16:13 -0700

    Added Read.cshtml and Read.cshtml.cs to Project

---
+++ src/Pages/Product/Read.cshtml.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+namespace ContosoCrafts.WebSite.Pages.Product
+{
+
+}
