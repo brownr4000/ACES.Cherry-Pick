a2dec4f522038b077856570780e0a2f4c78305e4
Bakyadevi Ray <91570270+bakyadevi@users.noreply.github.com>
2021-10-19 13:23:02 -0700

    Updated code in Read.cshtml and Read.cshtml.cs to display resturant page

--- src/Pages/Product/Read.cshtml.cs
+++ src/Pages/Product/Read.cshtml.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using System.Linq;
 
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,5 +7,31 @@ using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
+    public class ReadModel : PageModel
+    {
+        // Data middletier
+        public JsonFileProductService ProductService { get; }
 
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="productService"></param>
+        public ReadModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        // The data to show
+        public ProductModel Product;
+
+        /// <summary>
+        /// REST Get request
+        /// </summary>
+        /// <param name="id"></param>
+        public void OnGet(string id)
+        {
+            Product = ProductService.GetProducts().FirstOrDefault(m => m.Id.Equals(id));
+        }
+    }
 }
