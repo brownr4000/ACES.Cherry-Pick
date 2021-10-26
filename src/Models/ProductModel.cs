6e08e6ac97083bc67b963ad593de93a06c0f00ec
Robert Brown <rbrown3@seattleu.edu>
2021-10-26 15:17:12 -0700

    Added initial comments to ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -3,18 +3,34 @@ using System.Text.Json.Serialization;
 
 namespace ContosoCrafts.WebSite.Models
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class ProductModel
     {
+        // String ID
         public string Id { get; set; }
+        
+        // String Maker
         public string Maker { get; set; }
         
+        // Taking JSON property img and setting it to Image
         [JsonPropertyName("img")]
         public string Image { get; set; }
+
+        // String URL
         public string Url { get; set; }
+
+        // String Title
         public string Title { get; set; }
+
+        // String Description
         public string Description { get; set; }
+
+        // Int array Ratings
         public int[] Ratings { get; set; }
 
+        // Override ToString() method
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
 
  
