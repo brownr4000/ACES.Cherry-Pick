a39c3271ccc6635d31d1fd45a99239758c6dd155
Robert Brown <rbrown3@seattleu.edu>
2021-10-26 15:53:02 -0700

    Added initial comments to RestaurantModel.cs

--- src/Models/RestaurantModel.cs
+++ src/Models/RestaurantModel.cs
@@ -3,20 +3,35 @@ using System.Text.Json.Serialization;
 
 namespace ContosoCrafts.WebSite.Models
 {
+    /// <summary>
+    /// The RestaurantModel class creates the variables to serialize to and deserialize from the products.json file
+    /// </summary>
     public class RestaurantModel
     {
+        //
         public string Id { get; set; }
+        
+        //
         public string Maker { get; set; }
         
+        //
         [JsonPropertyName("img")]
         public string Image { get; set; }
+        
+        //
         public string Url { get; set; }
+        
+        //
         public string Title { get; set; }
+        
+        //
         public string Description { get; set; }
+        
+        //
         public int[] Ratings { get; set; }
 
+        //
         public override string ToString() => JsonSerializer.Serialize<RestaurantModel>(this);
 
- 
     }
 }
