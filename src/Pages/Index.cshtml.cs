6c3f010639f0d8554ff1be20a0a0287fe3bb980e
Robert Brown <rbrown3@seattleu.edu>
2021-10-26 16:24:42 -0700

    Updated comments to Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -5,33 +5,26 @@ using Microsoft.Extensions.Logging;
 
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
-/// <summary>
-/// I love Seattle Florence trying
-/// </summary>
+
 /// /// <summary>
-/// I love Seattle spring
-/// </summary>
-/// /// <summary>
-/// I love Seattle
+/// ACES team loves Seattle restaurants!
 /// </summary>
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary>
     /// Bakyadevi Ray
-    /// </summary>
-    ///
-    ///<summary>
     /// ZiQi Li
-    /// </summary>
-    ///
-    /// <summary>
     /// Florence Xie
-    /// hiii
+    /// Bob Brown
+    /// 
+    /// The IndexModel class
     /// </summary>
     public class IndexModel : PageModel
     {
+        //
         private readonly ILogger<IndexModel> _logger;
 
+        //
         public IndexModel(ILogger<IndexModel> logger,
             JsonFileProductService productService)
         {
@@ -39,9 +32,13 @@ namespace ContosoCrafts.WebSite.Pages
             ProductService = productService;
         }
 
+        //
         public JsonFileProductService ProductService { get; }
+        
+        //
         public IEnumerable<ProductModel> Products { get; private set; }
 
+        //
         public void OnGet()
         {
             Products = ProductService.GetAllData();
