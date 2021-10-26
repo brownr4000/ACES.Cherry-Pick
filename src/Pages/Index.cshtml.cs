0b533f687271d7fe7b4afe4f2184c0c538c4f192
Robert Brown <rbrown3@seattleu.edu>
2021-10-26 16:39:20 -0700

    Updated comments in Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -21,10 +21,10 @@ namespace ContosoCrafts.WebSite.Pages
     /// </summary>
     public class IndexModel : PageModel
     {
-        //
+        // Creates a private logger interface for the IndexModel class
         private readonly ILogger<IndexModel> _logger;
 
-        //
+        // Creates a IndexModel object
         public IndexModel(ILogger<IndexModel> logger,
             JsonFileProductService productService)
         {
@@ -32,13 +32,13 @@ namespace ContosoCrafts.WebSite.Pages
             ProductService = productService;
         }
 
-        //
+        // Creates the ProductService to read data from the JSON file
         public JsonFileProductService ProductService { get; }
         
-        //
+        // Creates an enermerable object based on the ProductModel
         public IEnumerable<ProductModel> Products { get; private set; }
 
-        //
+        // The OnGet method returns all the data from the ProductService
         public void OnGet()
         {
             Products = ProductService.GetAllData();
