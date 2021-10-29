68eadb6d82b82907f05cab438909d13a92029546
Robert Brown <rbrown3@seattleu.edu>
2021-10-28 23:50:26 -0700

    Added comments to Privacy.cshtml.cs

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -3,17 +3,29 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class PrivacyModel : PageModel
     {
+        //
         private readonly ILogger<PrivacyModel> _logger;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="logger"></param>
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public void OnGet()
         {
+
         }
     }
 }
