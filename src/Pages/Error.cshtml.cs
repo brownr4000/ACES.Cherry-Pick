3d5f1c25fe525117e1e72812aae04073501eb040
Robert Brown <rbrown3@seattleu.edu>
2021-10-28 23:45:53 -0700

    Added comments to Error.cshtml.cs

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -6,20 +6,33 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// 
+    /// </summary>
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
+        //
         public string RequestId { get; set; }
 
+        //
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
+        //
         private readonly ILogger<ErrorModel> _logger;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="logger"></param>
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
