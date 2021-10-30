01ee23b13794ab34205346079484aad4b7d5d1b4
Robert Brown <rbrown3@seattleu.edu>
2021-10-29 23:01:36 -0700

    Updated comments in Privacy.cshtml.cs

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -4,15 +4,15 @@ using Microsoft.Extensions.Logging;
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary>
-    /// 
+    /// The PrivacyModel class is a derived class from the RazorPages PageModel
     /// </summary>
     public class PrivacyModel : PageModel
     {
-        //
+        // A variable to hold logging for the PrivacyModel
         private readonly ILogger<PrivacyModel> _logger;
 
         /// <summary>
-        /// 
+        /// The PrivacyModel constructor creates a PrivacyModel object with the passed in log
         /// </summary>
         /// <param name="logger"></param>
         public PrivacyModel(ILogger<PrivacyModel> logger)
@@ -21,7 +21,7 @@ namespace ContosoCrafts.WebSite.Pages
         }
 
         /// <summary>
-        /// 
+        /// The OnGet method is empty
         /// </summary>
         public void OnGet()
         {
