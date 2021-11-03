2a19803abb9df77cf06aefe839d0b1e4a1524eb4
Bakyadevi Ray <91570270+bakyadevi@users.noreply.github.com>
2021-11-02 23:33:05 -0700

    Added unit test Patch_Valid_Should_Add_Ratings() in ProductsController.Tests.cs

--- UnitTests/Controllers/ProductsController.Tests.cs
+++ UnitTests/Controllers/ProductsController.Tests.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using ContosoCrafts.WebSite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace UnitTests.Controllers
 {
@@ -40,5 +41,31 @@ namespace UnitTests.Controllers
 
         }
         #endregion OnGet
+
+        #region Patch
+        [Test]
+        public void Patch_Valid_Should_Add_Ratings()
+        {
+            // Arrange
+
+            // Get the First data item
+            var data = TestHelper.ProductService.GetAllData().First();
+            var countOriginal = data.Ratings.Length;
+
+            var ratingReq = new ProductsController.RatingRequest();
+            ratingReq.ProductId = data.Id;
+            ratingReq.Rating = 5;
+
+            // Act 
+            var actionRes = pController.Patch(ratingReq);
+            var dataNewList = TestHelper.ProductService.GetAllData().First();
+
+            // Assert
+            Assert.IsTrue(actionRes.GetType().Equals(typeof(OkResult)));
+            Assert.AreEqual(countOriginal + 1, dataNewList.Ratings.Length);
+            Assert.AreEqual(5, dataNewList.Ratings.Last());
+
+        }
+        #endregion Patch
     }
 }
