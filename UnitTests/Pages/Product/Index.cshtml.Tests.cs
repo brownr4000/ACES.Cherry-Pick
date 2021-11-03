2d20040e4e02c80c278d95420bb5441a8d20694c
Robert Brown <rbrown3@seattleu.edu>
2021-11-02 23:52:18 -0700

    Updated OnGet_Valid_Should_Return_Products test in Index.cshtml.Tests.cs

--- UnitTests/Pages/Product/Index.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Index.cshtml.Tests.cs
@@ -83,7 +83,7 @@ namespace UnitTests.Pages.Product.Index
 
             // Assert
             Assert.AreEqual(true, pageModel.ModelState.IsValid);
-            Assert.AreEqual(17, pageModel.Products.ToList().Count);
+            Assert.AreEqual(18, pageModel.Products.ToList().Count);
         }
         #endregion OnGet
     }
