25d7cb6eb9883ea1cb60adf7de19a70b74ebf778
Robert Brown <rbrown3@seattleu.edu>
2021-11-07 00:42:51 -0700

    Created ToString Unit Test in ProductModel.Tests.cs

---
+++ UnitTests/Models/ProductModel.Tests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Models;
+using Castle.Core.Internal;
+
+namespace UnitTests
+{
+    class ProducttModelTests
+    {
+        #region TestSetup
+        public string TEST_WORDS = "Test";
+        public string CHECK_WORDS = "Check";
+
+        [SetUp]
+        public void TestInitialize()
+        {
+
+        }
+
+        #endregion TestSetup
+
+        #region ToStringTest
+        [Test]
+        public void ProductModel_Valid_ToString_Should_Return_True()
+        {
+            // Arrange
+            ProductModel thingModel = new ProductModel()
+            {
+                Id = TEST_WORDS,
+                Title = TEST_WORDS,
+                Description = TEST_WORDS,
+                Url = TEST_WORDS,
+                Image = TEST_WORDS,
+                Maker = TEST_WORDS,
+            };
+
+            // Act
+            var result = thingModel.ToString();
+
+            // Assert
+            Assert.AreEqual(false, result.IsNullOrEmpty());
+        }
+        #endregion ToStringTest
+    }
+}
