3b6ff49203898d3d3d06839dbb415527f95d237e
eblie <zli3@seattleu.edu>
2021-11-09 16:12:41 -0800

    Added RestaurantModels.cs in Product

---
+++ src/Models/RestaurantModels.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    /// <summary>
+    /// The RestaurantModel class creates the variables to serialize to and deserialize from the products.json file
+    /// </summary>
+    public class RestaurantModel
+    {
+        // String ID to store ID property from products.json attribute
+        public string Id { get; set; }
+
+        // String Maker to store Maker property from products.json attribute 
+        public string Maker { get; set; }
+
+        // Customizing JSON property name "img", and using String Image to store the img property from products.json attribute
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+
+        // String Url to store Url property from products.json attribute 
+        public string Url { get; set; }
+
+        // String Title to store Title property from products.json attribute 
+        public string Title { get; set; }
+
+        // String Description to store Description property from products.json attribute 
+        public string Description { get; set; }
+
+        // Integer Array Ratings to store Ratings property from products.json attribute
+        public int[] Ratings { get; set; }
+
+        // Integer Quantity to store quantity property
+        public int Quantity { get; set; }
+
+        // Double Price to store price number property
+        public double Price { get; set; }
+
+        // Override ToString() method for class
+        public override string ToString() => JsonSerializer.Serialize<RestaurantModel>(this);
+
+    }
+}
