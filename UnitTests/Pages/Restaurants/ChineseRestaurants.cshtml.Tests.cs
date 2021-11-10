519b70960b6c852f21a1830a0289fcb7a1f8483b
eblie <zli3@seattleu.edu>
2021-11-09 19:55:38 -0800

    Added unit tests for ChineseRestaurants.cshtml.Tests.cs

--- UnitTests/Pages/Restaurants/ChineseRestaurants.cshtml.Tests.cs
+++ UnitTests/Pages/Restaurants/ChineseRestaurants.cshtml.Tests.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UnitTests.Pages.Product.Read
+{
+    public class ChineseRestaurantsTests
+    {
+        #region TestSetup
+        public static ChineseRestaurantModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new ChineseRestaurantModel(TestHelper.ProductService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet("jenlooper-cactus");
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual("Chinese Cuisine", pageModel.Product.Title);
+        }
+        #endregion OnGet
+    }
+}
