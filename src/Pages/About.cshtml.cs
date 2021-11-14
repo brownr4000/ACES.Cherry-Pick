84a17a5615024481be5cb97e1e922a92189577e1
Robert Brown <rbrown3@seattleu.edu>
2021-11-14 01:32:50 -0800

    Updating formatting per Coding Standards in About.csthml.cs

--- src/Pages/About.cshtml.cs
+++ src/Pages/About.cshtml.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
@@ -30,6 +25,7 @@ namespace ContosoCrafts.WebSite.Pages
         /// </summary>
         public void OnGet()
         {
+
         }
     }
 }
