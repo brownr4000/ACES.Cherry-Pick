21bcfaad635d83b15863c7a6d6127740c555ee29
Robert Brown <rbrown3@seattleu.edu>
2021-11-14 17:45:29 -0800

    Added initial comments to ProductModel.Tests.cs

--- UnitTests/Models/ProductModel.Tests.cs
+++ UnitTests/Models/ProductModel.Tests.cs
@@ -1,16 +1,22 @@
 ﻿using NUnit.Framework;
-
 using ContosoCrafts.WebSite.Models;
 using Castle.Core.Internal;
 
 namespace UnitTests.Models
 {
+    /// <summary>
+    /// 
+    /// </summary>
     class ProducttModelTests
     {
         #region TestSetup
+        //
         public string TEST_WORDS = "Test";
         public string CHECK_WORDS = "Check";
 
+        /// <summary>
+        /// 
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
@@ -20,10 +26,14 @@ namespace UnitTests.Models
         #endregion TestSetup
 
         #region ToStringTest
+        /// <summary>
+        /// 
+        /// </summary>
         [Test]
         public void ProductModel_Valid_ToString_Should_Return_True()
         {
             // Arrange
+            //
             ProductModel thingModel = new ProductModel()
             {
                 Id = TEST_WORDS,
