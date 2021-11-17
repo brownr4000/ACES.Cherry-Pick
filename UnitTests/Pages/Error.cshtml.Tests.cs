4d34fd7862d8f7cf0c473e45393ba431f2bbaa13
Robert Brown <rbrown3@seattleu.edu>
2021-11-16 23:31:07 -0800

    Updated comments in Error.cshtml.Tests.cs

--- UnitTests/Pages/Error.cshtml.Tests.cs
+++ UnitTests/Pages/Error.cshtml.Tests.cs
@@ -1,21 +1,23 @@
 using System.Diagnostics;
-
 using Microsoft.Extensions.Logging;
-
 using NUnit.Framework;
-
 using Moq;
-
 using ContosoCrafts.WebSite.Pages;
 
 namespace UnitTests.Pages.Error
 {
+    /// <summary>
+    /// The ErrorTests class defines unit tests to cover the ErrorModel class
+    /// </summary>
     public class ErrorTests
     {
         #region TestSetup
-        
+        // Initialize ErrorModel object
         public static ErrorModel pageModel;
 
+        /// <summary>
+        /// The TestInitialize method creates the necessary objects for the initialization of the unit tests
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
@@ -29,10 +31,12 @@ namespace UnitTests.Pages.Error
                 TempData = TestHelper.TempData,
             };
         }
-
         #endregion TestSetup
 
         #region OnGet
+        /// <summary>
+        /// The OnGet_Valid_Activity_Set_Should_Return_RequestId method tests the functionality of the ErrortModel OnGet method
+        /// </summary>
         [Test]
         public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
         {
@@ -52,6 +56,9 @@ namespace UnitTests.Pages.Error
             Assert.AreEqual(activity.Id, pageModel.RequestId);
         }
 
+        /// <summary>
+        /// The OnGet_InValid_Activity_Null_Should_Return_TraceIdentifier method tests the functionality of the ErrortModel OnGet method
+        /// </summary>
         [Test]
         public void OnGet_InValid_Activity_Null_Should_Return_TraceIdentifier()
         {
