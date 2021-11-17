8e7bd393a0746ae049b7ab4c8e3af4b833ccc2fa
Robert Brown <rbrown3@seattleu.edu>
2021-11-16 22:45:12 -0800

    Updated comments in ProductModel.Tests.cs

--- UnitTests/Models/ProductModel.Tests.cs
+++ UnitTests/Models/ProductModel.Tests.cs
@@ -5,17 +5,17 @@ using Castle.Core.Internal;
 namespace UnitTests.Models
 {
     /// <summary>
-    /// 
+    /// The ProductModelTests class defines unit tests to cover the ProductModel class
     /// </summary>
     class ProducttModelTests
     {
         #region TestSetup
-        //
+        // Constant stings to hold values for determining unit test
         public string TEST_WORDS = "Test";
         public string CHECK_WORDS = "Check";
 
         /// <summary>
-        /// 
+        /// The TestInitialize method creates the necessary objects for the initialization of the unit tests
         /// </summary>
         [SetUp]
         public void TestInitialize()
@@ -27,13 +27,13 @@ namespace UnitTests.Models
 
         #region ToStringTest
         /// <summary>
-        /// 
+        /// The ProductModel_Valid_ToString_Should_Return_True tests the functionality of the overridden ToString method
         /// </summary>
         [Test]
         public void ProductModel_Valid_ToString_Should_Return_True()
         {
             // Arrange
-            //
+            // Creating a ProductModel with test values
             ProductModel thingModel = new ProductModel()
             {
                 Id = TEST_WORDS,
