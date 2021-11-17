2712dc8a9cd8d4b59b2ef4b3d496962c2af3bf9f
Bakyadevi Ray <91570270+bakyadevi@users.noreply.github.com>
2021-11-16 22:21:41 -0800

    Added codes to CommentModel.cs

--- src/Models/CommentModel.cs
+++ src/Models/CommentModel.cs
@@ -10,5 +10,10 @@ namespace ContosoCrafts.WebSite.Models
     /// </summary>
     public class CommentModel
     {
+        // The ID for this comment, use a Guid so it is always unique
+        public string Id { get; set; } = System.Guid.NewGuid().ToString();
+
+        // The Comment
+        public string Comment { get; set; }
     }
 }
