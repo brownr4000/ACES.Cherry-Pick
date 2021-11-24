6ffd1f4a77e0bbebaef1de77fbddd4be067bad50
Robert Brown <rbrown3@seattleu.edu>
2021-11-23 17:57:03 -0800

    Updated OnGet to implement defensive programming in Update.cshtml.cs

--- src/Pages/Product/Update.cshtml.cs
+++ src/Pages/Product/Update.cshtml.cs
@@ -36,9 +36,16 @@ namespace ContosoCrafts.WebSite.Pages.Product
         /// Loads the Data
         /// </summary>
         /// <param name="id"></param>
-        public void OnGet(string id)
+        public IActionResult OnGet(string id)
         {
             Product = ProductService.GetAllData().FirstOrDefault(m => m.Id.Equals(id));
+
+            // Checks if Product is Null
+            if (Product == null)
+            {
+                return RedirectToPage("./Index");
+            }
+            return Page();
         }
 
         /// <summary>
