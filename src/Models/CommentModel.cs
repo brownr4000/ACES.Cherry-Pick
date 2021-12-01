e1d5092e3db4d2afdb0c862c3f8d53b3c4830394
Robert Brown <rbrown3@seattleu.edu>
2021-11-30 18:45:29 -0800

    Updating formating and comments in CommentModel.cs

--- src/Models/CommentModel.cs
+++ src/Models/CommentModel.cs
@@ -1,20 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ContosoCrafts.WebSite.Models
 {
     /// <summary>
-    /// Comments entered by the user about the Product
+    /// The CommentModel class creates the variables to add comments entered by the user about the Product
     /// </summary>
     public class CommentModel
     {
-        // The ID for this comment, use a Guid so it is always unique
+        // String ID for this comment, use a Guid so it is always unique
         public string Id { get; set; } = System.Guid.NewGuid().ToString();
 
-        // The Comment
+        // String Comment to store the comment from the user
         [StringLength(maximumLength: 256, MinimumLength = 8, ErrorMessage = "The Comment should have a length of more than {2} and less than {1}")]
         public string Comment { get; set; }
     }
